9f9d06b08ec036d5994a2f40264dd744ce4742f0
TomiGimenez <105307974+TomiGimenez@users.noreply.github.com>
2024-01-30 20:38:21 -0300

    Test Unitarios y de integracion

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+mongoose.connect("mongodb://127.0.0.1:27017/productos", {
+  // useUnifiedTopology: true,
+  // useNewUrlParser: true,
 });
 
 const LibroSchema = new mongoose.Schema({
